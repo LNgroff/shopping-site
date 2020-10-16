bebffcdc18d6a2af6c7c787bc70faba84433a9b5
LNgroff <LNgroff@gmail.com>
2020-10-16 16:25:51 +0000

    Made Customer Class
    Co-authored-by: kayteecannon <kaytee.cannon@me.com>

--- customers.py
+++ customers.py
@@ -4,4 +4,46 @@
 class Customer(object):
     """Ubermelon customer."""
 
-    # TODO: need to implement this
+    def __init__(self,
+                 first_name,
+                 last_name,
+                 email,
+                 password,
+                 
+                 ):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+
+        return f'<Customer: {self.first_name}, {self.last_name}, {self.email}>'
+
+def read_customers_from_file(filepath):
+    """Read melon type data and populate dictionary of melon types.
+
+    Dictionary will be {id: Melon object}
+    """
+
+    customers = {}
+
+    with open(filepath) as file:
+        for line in file:
+            (first_name,
+            last_name,
+            email,
+            password) = line.strip().split("|")
+    
+            customers[email] = Customer(first_name,
+                                        last_name,
+                                        email,
+                                        password)
+
+    return customers
+
+def get_by_email(email):
+
+    return customers_dict[email]
+
+customers_dict = read_customers_from_file("customers.txt")
